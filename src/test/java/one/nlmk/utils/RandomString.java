06291eed580be1b2f14afab0032bcbf597a67c64
VEvtushenko <evt-vladimir@yandex.ru>
2022-04-17 00:58:46 +0300

    random iniput with Faker

---
+++ src/test/java/one/nlmk/utils/RandomString.java
@@ -0,0 +1,32 @@
+package one.nlmk.utils;
+
+import java.util.Random;
+
+public class RandomString {
+    public static String getRandomPhone () {
+        Random r = new Random();
+        String strResult = "9";
+        for (int i = 0; i < 9; i++ ) {
+            int result = r.nextInt(9);
+            strResult = strResult + Integer.toString(result);
+        }
+        return strResult;
+    }
+
+    public static String getRandomMonth (int numberOfMonth) {
+        String[] listOfMonth = new String[] {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "Julie",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"};
+        return listOfMonth[numberOfMonth];
+    }
+}
