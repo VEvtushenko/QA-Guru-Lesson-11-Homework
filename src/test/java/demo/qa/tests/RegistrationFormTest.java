e51ef08e773cd78c0a717f8ae3e071c983a40b2c
VEvtushenko <evt-vladimir@yandex.ru>
2022-05-08 19:11:29 +0300

    Added TestBase and remote browser launch

--- src/test/java/demo/qa/tests/RegistrationFormTest.java
+++ src/test/java/demo/qa/tests/RegistrationFormTest.java
@@ -1,29 +1,13 @@
 package demo.qa.tests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import demo.qa.pages.RegistrationFormPage;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import java.util.Date;
-import java.util.Locale;
-import static java.lang.String.format;
-import com.github.javafaker.Faker;
-import static demo.qa.utils.RandomUtils.getRandomPhone;
-import static demo.qa.utils.RandomUtils.getRandomMonth;
 
-
-
-public class RegistrationFormTest {
-
-    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-
-    @BeforeAll
-    static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-    }
+@Tag("RegistrationFormTest")
+public class RegistrationFormTest extends TestBase {
 
     @DisplayName("Проверка заполнения регистрационной формы обучающегося")
     @Test
@@ -31,34 +15,6 @@ public class RegistrationFormTest {
 
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Faker faker = new Faker(new Locale("EN", "IND"));
-        Date birthDate = faker.date().birthday(18, 100);
-
-        String[] hobbie = new String[] {"Sports", "Reading", "Music"};
-        String[] gender = new String[] {"Male", "Female", "Other"};
-        String[] subjects = new String[] {"Economics", "Arts", "Computer Science", "Math"};
-        String[] states = new String[] {"NCR", "Uttar Pradesh", "Haryana", "Rajastan"};
-        String[] cities = new String[] {"Delhi", "Gurgaon", "Nodia",
-                                        "Agra", "Lucknow", "Merrut",
-                                        "Karnal", "Panipat",
-                                        "Jaipur", "Jaiselmer"};
-        String  firstName = faker.name().firstName(),
-                lastName = faker.name().lastName(),
-                email = faker.internet().emailAddress(),
-                mobileNumber =  getRandomPhone(),
-                address = faker.address().fullAddress(),
-                stringBirthDate = String.valueOf(birthDate),
-                birthYear = stringBirthDate.substring(stringBirthDate.length() - 4),
-                birthMonth = getRandomMonth(birthDate.getMonth()),
-                birthDay = String.valueOf(birthDate.getDay()),
-
-                expectedFullName = format("%s %s", firstName, lastName),
-                expectedDateOfBirth = format("%s %s,%s", birthDay,birthMonth, birthYear),
-                expectedStateAndCity = format("%s %s", states[0], cities[1]);
-
-        java.io.File imgStudent = new java.io.File("src/test/resources/Images/images.jpg");
-
-        // Вводим данные
         registrationFormPage.openPage()
                 .setFirstName(firstName)
                 .setLastName(lastName)
@@ -76,7 +32,6 @@ public class RegistrationFormTest {
                 .seleclCity(cities[1])
                 .submit();
 
-        // Проверяем результаты.
         registrationFormPage.checkWindowWithResults()
                 .checkResults("Student Name", expectedFullName, true)
                 .checkResults("Student Email", email, true)
@@ -89,6 +44,5 @@ public class RegistrationFormTest {
                 .checkResults("Picture", imgStudent.getName(), true)
                 .checkResults("Address", address, true)
                 .checkResults("State and City", expectedStateAndCity, true);
-
     }
 }
---
+++ src/test/java/demo/qa/tests/TestBase.java
@@ -0,0 +1,51 @@
+package demo.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import demo.qa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Date;
+import java.util.Locale;
+
+import static demo.qa.utils.RandomUtils.getRandomMonth;
+import static demo.qa.utils.RandomUtils.getRandomPhone;
+import static java.lang.String.format;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    Faker faker = new Faker(new Locale("EN", "IND"));
+    Date birthDate = faker.date().birthday(18, 100);
+    java.io.File imgStudent = new java.io.File("src/test/resources/Images/images.jpg");
+
+    String[] hobbie = new String[] {"Sports", "Reading", "Music"};
+    String[] gender = new String[] {"Male", "Female", "Other"};
+    String[] subjects = new String[] {"Economics", "Arts", "Computer Science", "Math"};
+    String[] states = new String[] {"NCR", "Uttar Pradesh", "Haryana", "Rajastan"};
+    String[] cities = new String[] {"Delhi", "Gurgaon", "Nodia",
+            "Agra", "Lucknow", "Merrut",
+            "Karnal", "Panipat",
+            "Jaipur", "Jaiselmer"};
+
+    String  firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            mobileNumber =  getRandomPhone(),
+            address = faker.address().fullAddress(),
+            stringBirthDate = String.valueOf(birthDate),
+            birthYear = stringBirthDate.substring(stringBirthDate.length() - 4),
+            birthMonth = getRandomMonth(birthDate.getMonth()),
+            birthDay = String.valueOf(birthDate.getDay()),
+
+            expectedFullName = format("%s %s", firstName, lastName),
+            expectedDateOfBirth = format("%s %s,%s", birthDay,birthMonth, birthYear),
+            expectedStateAndCity = format("%s %s", states[0], cities[1]);
+}
