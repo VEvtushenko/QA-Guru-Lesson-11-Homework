e4d7323cfed4390b67477126e97bae6a3ecf3aea
VEvtushenko <evt-vladimir@yandex.ru>
2022-05-09 20:01:46 +0300

    Close web driver

--- src/test/java/demo/qa/tests/TestBase.java
+++ src/test/java/demo/qa/tests/TestBase.java
@@ -1,7 +1,6 @@
 package demo.qa.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
 import demo.qa.helpers.Attach;
 import demo.qa.pages.RegistrationFormPage;
@@ -9,31 +8,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-import static demo.qa.utils.RandomUtils.getRandomMonth;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static demo.qa.utils.RandomUtils.getRandomPhone;
 import static java.lang.String.format;
 
 public class TestBase {
 
-    @BeforeAll
-    static void setUp() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-    }
-
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     Faker faker = new Faker(new Locale("EN", "IND"));
-    Date birthDate = faker.date().birthday(18, 100);
+
     java.io.File imgStudent = new java.io.File("src/test/resources/Images/images.jpg");
+
+    SimpleDateFormat dateFormater = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+    String stringBirthDate = dateFormater.format(faker.date().birthday(18, 70));
+    String[] birthDate = stringBirthDate.split(" ");
     String[] hobbie = new String[] {"Sports", "Reading", "Music"};
     String[] gender = new String[] {"Male", "Female", "Other"};
     String[] subjects = new String[] {"Economics", "Arts", "Computer Science", "Math"};
@@ -46,12 +38,11 @@ public class TestBase {
     String  firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
             email = faker.internet().emailAddress(),
-            mobileNumber =  getRandomPhone(),
+            mobileNumber =  faker.number().digits(10),
             address = faker.address().fullAddress(),
-            stringBirthDate = String.valueOf(birthDate),
-            birthYear = stringBirthDate.substring(stringBirthDate.length() - 4),
-            birthMonth = getRandomMonth(birthDate.getMonth()),
-            birthDay = String.valueOf(birthDate.getDate()),
+            birthYear = birthDate[2],
+            birthMonth = birthDate[1],
+            birthDay = birthDate[0],
             expectedFullName = format("%s %s", firstName, lastName),
             expectedDateOfBirth = format("%s %s,%s", birthDay,birthMonth, birthYear),
             expectedStateAndCity = format("%s %s", states[0], cities[1]);
@@ -61,9 +52,20 @@ public class TestBase {
         Attach.pageSource();
     }
 
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
     @AfterEach
     public void getVideoAndLog() {
         Attach.addVideo();
         Attach.browserConsoleLogs();
+        closeWebDriver();
     }
 }
